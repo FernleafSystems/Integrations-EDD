39b82c7399165b29ccb9a3a5d5dde52a9997f753
Paul G <paul@icontrolwp.com>
2019-06-24 10:34:03 +0100

    ensure limit cart to 1 now replaces existing items instead of limiting existing

--- src/Utilities/Features/LimitItemInCartToOne.php
+++ src/Utilities/Features/LimitItemInCartToOne.php
@@ -26,6 +26,7 @@ class LimitItemInCartToOne {
 		};
 
 		// After an item has been added to the cart, it'll traverse all items and remove duplicates
+		add_action( 'edd_pre_add_to_cart', [ $this, 'onPreAddToCart' ], 10, 1 );
 		add_action( 'edd_post_add_to_cart', [ $this, 'onAddToCart' ], 10, 1 );
 	}
 
@@ -38,6 +39,23 @@ class LimitItemInCartToOne {
 	}
 
 	/**
+	 * If we're about to add a new download to the cart and the Download ID already exists in the cart
+	 * we remove the existing item so that it will be replaced by the upcoming.
+	 * @param int $nDownloadId
+	 */
+	public function onPreAddToCart( $nDownloadId ) {
+
+		if ( $this->isEnabledLimitToOne( $nDownloadId ) ) {
+			foreach ( edd_get_cart_contents() as $nKey => $aItem ) {
+				if ( isset( $aItem[ 'id' ] ) && $nDownloadId === $aItem[ 'id' ] ) {
+					EDD()->cart->remove( $nKey );
+				}
+			}
+		}
+	}
+
+	/**
+	 * After add the the onPreAddToCart, this is probably no longer required.
 	 * @param int $nDownloadId
 	 */
 	public function onAddToCart( $nDownloadId ) {
